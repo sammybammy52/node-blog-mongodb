dc0dd000ec72a5b6935234a3336d685cca30afba
Sammy Bammy <sammybammystudios@gmail.com>
2022-04-23 18:06:46 +0100

    deployment modification

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 const dbURI = process.env.dbURI;
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then((result) => app.listen(3000))
+.then((result) => app.listen(process.env.PORT || 3000))
 .catch((err) => console.log(err));
 
 
